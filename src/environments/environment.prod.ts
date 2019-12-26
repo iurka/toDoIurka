98f4852e0df1a32bd7e50e632ba15bd3f924a4d6
iurie <iurie.nunu@mail.polimi.it>
2019-12-26 18:38:52 +0100

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCf7vZyjrXyfiNwBqk7XnqgqSzlirX6_yk",
+    authDomain: "todoiurka.firebaseapp.com",
+    databaseURL: "https://todoiurka.firebaseio.com",
+    projectId: "todoiurka",
+    storageBucket: "todoiurka.appspot.com",
+    messagingSenderId: "730713811513",
+    appId: "1:730713811513:web:76926add1516f9c1b83be8",
+    measurementId: "G-H3XQSPCJH9"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCf7vZyjrXyfiNwBqk7XnqgqSzlirX6_yk",
+    authDomain: "todoiurka.firebaseapp.com",
+    databaseURL: "https://todoiurka.firebaseio.com",
+    projectId: "todoiurka",
+    storageBucket: "todoiurka.appspot.com",
+    messagingSenderId: "730713811513",
+    appId: "1:730713811513:web:76926add1516f9c1b83be8",
+    measurementId: "G-H3XQSPCJH9"
+  }
 };
 
 /*
